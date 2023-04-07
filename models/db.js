2b6766a653566ed03b80e8c423493619bac7c724
AsynchronousAbhay <128599731+AsynchronousAbhay@users.noreply.github.com>
2023-04-08 01:17:39 +0530

    passport-nodemailer

---
+++ models/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+mongoose.connect('mongodb://127.0.0.1:27017/passportDB').then(() => {
+    console.log("connected!");
+})
+    .catch((err) => {
+        console.log(err.message);
+    });
+
